0e05fee19aeb1e43b4c65b1d4f06ec299e4231a8
kovalexand <kovalexander13@gmail.com>
2022-09-09 02:06:36 +0300

    dto update

--- project/auth/models/dto/registration.py
+++ project/auth/models/dto/registration.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
-from pydantic import BaseModel
+
+from pydantic import BaseModel, Field, EmailStr
 
 
-@dataclass
 class RegistrationRequest(BaseModel):
-    name: str
-    email: str
-    password: str
+    name: str = Field(min_length=1, max_length=15)
+    email: EmailStr
+    password: str = Field(min_length=6, max_length=20)
 
 
 @dataclass
-class RegistrationResponse(BaseModel):
+class RegistrationResponse:
     name: str
