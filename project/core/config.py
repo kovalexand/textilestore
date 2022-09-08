60cf8532f78b19eb5dac4f4fca737b1ca4d704c6
kovalexand <kovalexander13@gmail.com>
2022-09-08 12:40:48 +0300

    config init

---
+++ project/core/config.py
@@ -0,0 +1,33 @@
+from pydantic import BaseSettings
+from functools import cache
+
+
+class DBSettings(BaseSettings):
+    username: str
+    password: str
+    database: str
+    host: str
+    port: str
+
+    class Config:
+        env_prefix = "DB_"
+        env_file = ".env"
+
+
+class TokenSettings(BaseSettings):
+    access_token_expire_minutes: int
+    refresh_token_expire_minutes: int
+
+    class Config:
+        env_prefix = "TK_"
+        env_file = ".env"
+
+
+@cache
+def get_token_settings() -> TokenSettings:
+    return TokenSettings()
+
+
+@cache
+def get_db_settings() -> DBSettings:
+    return DBSettings()
