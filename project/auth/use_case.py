053d59da2a787aab75c66daac40dbc22cc68b646
kovalexand <kovalexander13@gmail.com>
2022-09-09 02:06:49 +0300

    use case update

--- project/auth/use_case.py
+++ project/auth/use_case.py
@@ -1,10 +1,36 @@
+import uuid
+import hashlib
 from abc import ABC, abstractmethod
+
+from project.auth.models.entities import User
+from project.auth.models.dto.registration import RegistrationRequest, RegistrationResponse
+from project.auth.repositories.user import IUserRepository
 
 
 class IAuthUseCase(ABC):
-    pass
+    @abstractmethod
+    async def registration(self, request: RegistrationRequest) -> RegistrationResponse:
+        """
+        :param request:
+        :return:
+        """
 
 
 class AuthUseCase(IAuthUseCase):
-    pass
+    def __init__(self, repo: IUserRepository):
+        self.repo = repo
 
+    @staticmethod
+    def get_hashed_password(password: str, salt: str) -> str:
+        return hashlib.sha512((password + salt).encode()).hexdigest()
+
+    @staticmethod
+    def validate_password(password: str, salt: str, hashed_password: str) -> bool:
+        return True if hashlib.sha512((password + salt).encode()).hexdigest() == hashed_password else False
+
+    async def registration(self, request: RegistrationRequest) -> RegistrationResponse:
+        salt = uuid.uuid4().hex
+        password = self.get_hashed_password(request.password, salt)
+        user = User(name=request.name, email=request.email, password=password, key=salt)
+        await self.repo.create(user)
+        return RegistrationResponse(name=request.name)
