3b127a9c6f1f1d4876a23c2ecc81ca3aaa370ee2
kovalexand <kovalexander13@gmail.com>
2022-09-08 22:41:53 +0300

    server update

--- project/server/app.py
+++ project/server/app.py
@@ -1,4 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Request
 
-# init fastAPI server
+from project.core.database import Base, engine
+from project.core.exceptions import ApplicationException
+
+# init services
+Base.metadata.create_all(bind=engine)
 app = FastAPI(debug=True)
+
+# init fastAPI`s routers
+
+
+# init fastAPI`s exceptions
+@app.exception_handler(ApplicationException)
+def app_exception_handler(request: Request, exc: ApplicationException) -> Response:
+    return Response(
+        status_code=exc.code_status,
+        content=exc.body
+    )
