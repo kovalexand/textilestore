9c80aea76763df7d12371bcc50dfdd559424f718
kovalexand <kovalexander13@gmail.com>
2022-09-17 00:49:28 +0300

    init new methods

--- project/auth/delivery/controller.py
+++ project/auth/delivery/controller.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends
 
 from project.auth.models.dto.login import LoginRequest, LoginResponse
+from project.auth.models.dto.password_change import PasswordChangeResponse, PasswordChangeRequest
 from project.auth.models.dto.refresh import RefreshResponse, RefreshRequest
+from project.auth.models.dto.send_verify_link import SendVerifyLinkResponse, SendVerifyLinkRequest
+from project.auth.models.dto.user_info import UserInfoResponse, UserInfoRequest
 from project.auth.use_case import IAuthUseCase
 from project.auth.dependencies import get_auth_depend
 from project.auth.models.dto.registration import RegistrationRequest, RegistrationResponse
@@ -22,3 +25,18 @@ async def login(request: LoginRequest, use_case: IAuthUseCase = Depends(get_auth
 @auth.post(path="/refresh_token", response_model=RefreshResponse)
 async def refresh_token(request: RefreshRequest, use_case: IAuthUseCase = Depends(get_auth_depend)):
     return await use_case.refresh(request)
+
+
+@auth.post(path="/password_change", response_model=PasswordChangeResponse)
+async def password_change(request: PasswordChangeRequest, use_case: IAuthUseCase = Depends(get_auth_depend)):
+    return await use_case.password_change(request)
+
+
+@auth.post(path="/user_info", response_model=UserInfoResponse)
+async def get_user_info(request: UserInfoRequest, use_case=Depends(get_auth_depend)):
+    return await use_case.get_user_info(request)
+
+
+@auth.post(path="/send_verify_link", response_model=SendVerifyLinkResponse)
+async def send_verify_link(request: SendVerifyLinkRequest, use_case=Depends(get_auth_depend)):
+    return await use_case.send_verify_link(request)
--- project/auth/use_case.py
+++ project/auth/use_case.py
@@ -4,7 +4,10 @@ from abc import ABC, abstractmethod
 
 from project.auth.exceptions import UserDoesNotExistException
 from project.auth.models.dto.login import LoginRequest, LoginResponse
+from project.auth.models.dto.password_change import PasswordChangeRequest, PasswordChangeResponse
 from project.auth.models.dto.refresh import RefreshRequest, RefreshResponse
+from project.auth.models.dto.send_verify_link import SendVerifyLinkRequest, SendVerifyLinkResponse
+from project.auth.models.dto.user_info import UserInfoRequest, UserInfoResponse
 from project.auth.models.entities import User
 from project.auth.models.dto.registration import RegistrationRequest, RegistrationResponse
 from project.auth.repositories.user import IUserRepository
@@ -33,11 +36,32 @@ class IAuthUseCase(ABC):
         :return:
         """
 
+    @abstractmethod
+    async def password_change(self, request: PasswordChangeRequest) -> PasswordChangeResponse:
+        """
+        :param request:
+        :return:
+        """
+
+    @abstractmethod
+    async def get_user_info(self, request: UserInfoRequest) -> UserInfoResponse:
+        """
+        :param request:
+        :return:
+        """
+
+    @abstractmethod
+    async def send_verify_link(self, request: SendVerifyLinkRequest) -> SendVerifyLinkResponse:
+        """
+        :param request:
+        :return:
+        """
+
 
 class AuthUseCase(IAuthUseCase):
-    def __init__(self, repo: IUserRepository, token: ITokenService):
-        self.repo = repo
-        self.token_service = token
+    def __init__(self, user: IUserRepository, token: ITokenService):
+        self.user = user
+        self.token = token
 
     @staticmethod
     def get_hashed_password(password: str, salt: str) -> str:
@@ -51,17 +75,35 @@ class AuthUseCase(IAuthUseCase):
         salt = uuid.uuid4().hex
         password = self.get_hashed_password(request.password, salt)
         user = User(name=request.name, email=request.email, password=password, key=salt)
-        await self.repo.create(user)
+        await self.user.create(user)
         return RegistrationResponse(name=request.name)
 
     async def login(self, request: LoginRequest) -> LoginResponse:
-        user = await self.repo.get_user(request.email)
+        user = await self.user.find_by_email(request.email)
         if not user or not self.validate_password(request.password, user.key, user.password):
             exp_body = {'detail': 'email or password uncorrected'}
             raise UserDoesNotExistException(body=exp_body)
-        token = await self.token_service.create_new_token(user_id=str(user.id))
-        return LoginResponse(name=str(user.name), token=token)
+        token = await self.token.create(qualifier=str(user.id))
+        return LoginResponse(name=str(user.name), tokens=token)
 
     async def refresh(self, request: RefreshRequest) -> RefreshResponse:
-        token = await self.token_service.refresh_token(request.token)
-        return RefreshResponse(token=token)
+        token = await self.token.refresh(request.tokens)
+        return RefreshResponse(tokens=token)
+
+    async def password_change(self, request: PasswordChangeRequest) -> PasswordChangeResponse:
+        _id = await self.token.authentication(request.tokens)
+        user = await self.user.find_by_id(_id)
+        password = self.get_hashed_password(request.password, user.key)
+        await self.user.password_change(user, password)
+        return PasswordChangeResponse(detail="password is changed")
+
+    async def get_user_info(self, request: UserInfoRequest) -> UserInfoResponse:
+        _id = await self.token.authentication(request.tokens)
+        user = await self.user.find_by_id(_id)
+        name = str(user.name)
+        email = str(user.email)
+        is_verified = bool(user.is_verified)
+        return UserInfoResponse(name=name, email=email, is_verified=is_verified)
+
+    async def send_verify_link(self, request: SendVerifyLinkRequest) -> SendVerifyLinkResponse:
+        return SendVerifyLinkResponse(detail="check your email")
